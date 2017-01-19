e23a99779635ce49ffb1c321dce3922a24c7193d
Dmitrii <dima_sh84@mail.ru>
2017-01-19 23:38:31 +0200

    creating our subcomponent

---
+++ 04-creating our subcomponent/src/app/AddAppointment.js
@@ -0,0 +1,52 @@
+var React = require('react');
+
+var AddAppointment = React.createClass({
+    render: function() {
+        return (
+            <div className="panel panel-primary">
+                <div className="panel-heading apt-addheading">
+                    <span className="glyphicon glyphicon-plus"></span> Add appointment
+                </div>
+                <div className="panel-body">
+                    <form className="add-appointment form-horizontal">
+                        <div className="form-group">
+                            <label className="col-sm-2 control-label" htmlFor="petName">Pet Name</label>
+                            <div className="col-sm-10">
+                                <input type="text" className="form-control" id="petName" ref="inputPetName" placeholder="Pet's name" />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label className="col-sm-2 control-label" htmlFor="petOwner">Pet Owner</label>
+                            <div className="col-sm-10">
+                                <input type="text" className="form-control" id="petOwner" ref="inputPetOwner" placeholder="Owner's name" />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label className="col-sm-2 control-label" htmlFor="aptDate">Date</label>
+                            <div className="col-sm-4">
+                                <input type="date" className="form-control" id="aptDate" ref="inputAptDate" />
+                            </div>
+                            <label className="col-sm-2 control-label" htmlFor="aptTime">Time</label>
+                            <div className="col-sm-4">
+                                <input type="time" className="form-control" id="aptTime" ref="inputAptTime" />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <label className="col-sm-2 control-label" htmlFor="aptNotes">Apt. Notes</label>
+                            <div className="col-sm-10">
+                                <textarea className="form-control" rows="4" cols="50" id="aptNotes" ref="inputAptNotes" />
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <div className="col-sm-2-offset col-sm-10">
+                                <button type="submit" className="btn btn-primary pull-right">Add appointment</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+});
+
+module.exports = AddAppointment;
---
+++ 04-creating our subcomponent/src/app/index.js
@@ -0,0 +1,58 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var _ = require('lodash');
+
+var AptList = require('./AptList');
+var AddAppointment = require('./AddAppointment');
+
+var MainInterface = React.createClass({
+    getInitialState: function() {
+        return {
+           myAppointments: []
+        }
+    },
+
+    componentDidMount: function() {
+      this.serverRequest = $.get('./app/data.json', function(result) {
+            var tempApts = result;
+            this.setState({
+                myAppointments: tempApts
+            });
+        }.bind(this));
+    },
+
+    componentWillUnmount: function() {
+        this.serverRequest.abort();
+    },
+
+    deleteMessage: function(item) {
+        var allApts = this.state.myAppointments;
+        var newApts = _.without(allApts, item);
+        this.setState({
+            myAppointments: newApts
+        });
+    },
+
+    render: function() {
+        var filteredApts = this.state.myAppointments;
+        
+        filteredApts = filteredApts.map(function(item, index) {
+            return (
+                <AptList
+                    key={index}
+                    singleItem={item}
+                    whichItem={item}
+                    onDelete={this.deleteMessage} />
+            )
+        }.bind(this));
+
+        return (
+            <div className="interface">
+                <AddAppointment />
+                <ul className="item-list media-list">{filteredApts}</ul>
+            </div>
+        )
+    }
+});
+
+ReactDOM.render(<MainInterface />, document.getElementById('petAppointments'));
